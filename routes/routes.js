95427b4c56535194d051823468b875c1a856e213
Uzuigwe Gideon <uptodateacademy@outlook.com>
2024-05-18 00:12:01 +0100

    Version 1.0.2

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,27 @@
 const router = require("express").Router();
 
+//Redirect User 
 router.get("/", (req, res, next) => {
     res.redirect("/en/home");
+    next();
 });
 
+//Gets Homepage
 router.get("/en/home", (req, res, next) => {
-    res.render("index", { title: "Shop | eCommerce" });
+    res.render("index", { title: "Aroma Shop | eCommerce" });
+    next();
 });
+
+//Gets The Login Page
+router.get("/en/login", (req, res, next) => {
+    res.render("login", { title: "Aroma Shop | Login" });
+    next();
+});
+
+//Gets The Sign Up Page
+router.get("/en/signup", (req, res, next) => {
+    res.render("signup", { title: "Aroma Shop | Create Account" });
+    next();
+})
 
 module.exports = router;
